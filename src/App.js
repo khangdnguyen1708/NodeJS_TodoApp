a17b23ddefa8a9529584cc6d221447e2d9ccad7d
khangdnguyen1708 <kdnguyen1708@gmail.com>
2023-05-08 19:22:30 +0700

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import List from './components/List';
 function App() {
   return (
     <div className="App">
+      <h1>hello</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <List itemList={["Get Milk", "Buy Amazon", "Take over the world"]} />
